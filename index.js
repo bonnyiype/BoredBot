085f0fb166753496366b009576bed568f6e8100b
bonnyiype <105000155+bonnyiype@users.noreply.github.com>
2023-02-28 15:28:09 +0000

    Main - Changes

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
-fetch("https://apis.scrimba.com/bored/api/activity")
-.then(response => response.json())
-.then(data => {document.write(data))
+const getActivityButton = document.getElementById("getActivityButton");
+getActivityButton.addEventListener("click", getRandomActivity);
 
-}
+function getRandomActivity(){ fetch("https://apis.scrimba.com/bored/api/activity")
+.then((response) => response.json())
+  .then((data) => {
+  
+    document.getElementById("activity").textContent = data.activity
+
+
+  })};
+
